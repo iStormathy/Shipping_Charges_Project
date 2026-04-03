2672273e2a7f6f0ba1607e99e4da649436077117
iStormathy <stormageddon473@yahoo.com>
2026-04-03 10:14:26 -0400

    Added comments explaining the code

--- Shipping_Charges_Project/Shipping_Charges_Project.cpp
+++ Shipping_Charges_Project/Shipping_Charges_Project.cpp
@@ -1,3 +1,5 @@
+// This program calculates the rate of a delivery
+// based on weight and distance travelled
 #include <iostream>
 #include <cmath>
 #include <iomanip>
@@ -7,7 +9,7 @@ int main()
 	double weight, distance, rate;
 	cout << "Enter the weight (in kg's) of your package: ";
 	cin >> weight;
-	if (weight <= 0 || weight > 20)
+	if (weight <= 0 || weight > 20)	// Limits weight to 20 kg's
 		cout << "Package to heavy, cannot ship.";
 	else
 	{
@@ -21,13 +23,14 @@ int main()
 			rate = 3.70;
 		else
 			rate = 4.80;
-		cout << "How far (in miles) is the destination?: ";
+		cout << "Enter distance (in miles): ";
 		cin >> distance;
-		if (distance <= 0 || distance > 3000)
+		if (distance <= 0 || distance > 3000) // Limits distance to 3000 miles
 			cout << "Destination too far, cannot ship.";
 		else
 		{
-			double finalPrice = rate*(trunc(distance / 500) +1);
+			double finalPrice = rate*(trunc(ceil(distance) / 500) +1);
+			//"distance / 500" is truncated in order to prevent rollover into higher price bracket
 			cout << "Final Price: $" << fixed << setprecision(2) << finalPrice;
 		}
 	}
